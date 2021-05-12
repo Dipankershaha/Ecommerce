907d151e7ba008c9b821c135f691dda106c14609
Dipankershaha <dipankershaha@gmail.com>
2021-05-12 23:46:10 +0600

    frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Navbar, Nav, Container,NavDropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap';
+import SearchBox from './SearchBox';
+import { logout } from '../actions/userActions';
+
+const Header = () => {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = ()=>{
+        dispatch(logout())
+    }
+
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+                <Container>
+                    <LinkContainer to="/">
+                    <Navbar.Brand>Ecommerce</Navbar.Brand>
+                    </LinkContainer>
+                    
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <SearchBox/>
+                        <Nav className="ml-auto">
+                        <LinkContainer to="/cart">
+                        <Nav.Link> <i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                        </LinkContainer>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to="/profile">
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ):(
+                            <LinkContainer to="/login">
+                                <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+                            </LinkContainer>
+                        )}
+
+
+                        {userInfo && userInfo.isAdmin && (
+                            <NavDropdown title='Admin' id='adminmenu'>
+                                <LinkContainer to="/admin/userlist">
+                                    <NavDropdown.Item>Users</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/admin/productlist">
+                                    <NavDropdown.Item>Products</NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/admin/orderlist">
+                                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                                </LinkContainer>
+                            </NavDropdown>
+
+                        )}
+
+                        
+                        
+                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown> */}
+                        </Nav>
+                        {/* <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                        </Form> */}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
